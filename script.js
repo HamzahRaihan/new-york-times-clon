298ccc1e0fc366e0df0ea60567bc86666a428a14
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-09-25 16:11:05 +0700

    feat: added footer

--- script.js
+++ script.js
@@ -2,76 +2,12 @@ import API_KEY from './apiKey.js';
 
 const searchButton = document.querySelector('.search-button');
 const container = document.querySelector('.news-container');
-const loading = document.querySelector('.loading');
-console.log(loading);
 
 searchButton.addEventListener('click', () => {
   const inputKeyword = document.querySelector('.input-keyword').value;
   const search = searchNews(inputKeyword);
   showSearchNews(search);
-  console.log(inputKeyword);
-  console.log(searchButton);
 });
-
-const navLink = document.querySelectorAll('.btn-light');
-navLink.forEach((m) => {
-  m.addEventListener('click', () => {
-    const section = m.id;
-    getNewsBySection(section);
-  });
-});
-// Home Section News
-async function getHomeNews() {
-  const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`);
-  const homeNews = await response.json();
-  let cards = '';
-  homeNews.results.forEach((m) => (cards += showHomeNews(m)));
-  container.innerHTML = cards;
-  console.log(homeNews.results);
-}
-
-async function getNewsBySection(section) {
-  loading.style.display = 'block';
-  console.log('🚀 ~ file: script.js:35 ~ getNewsBySection ~ loading:', loading);
-  try {
-    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`);
-    if (!response.ok) {
-      throw new Error('Something went wrong');
-    } else {
-      const news = await response.json();
-      let cards = '';
-      news.results.forEach((m) => (cards += showHomeNews(m)));
-      container.innerHTML = cards;
-    }
-  } catch (error) {
-    console.log(error);
-  } finally {
-    loading.style.display = 'none';
-  }
-}
-
-getHomeNews();
-
-// Search News
-async function searchNews(keyword) {
-  const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${API_KEY}`);
-  const searchList = await response.json();
-  let cards = '';
-  searchList.response.docs.forEach((m) => (cards += showSearchNews(m)));
-  container.innerHTML = cards;
-}
-
-// Get Date
-const dateContainer = document.querySelector('.current-date');
-
-const options = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  weekday: 'long',
-};
-const currentDate = new Date().toLocaleDateString('en-US', options);
-dateContainer.innerHTML = currentDate;
 
 // format Date
 const formatDate = (date) => {
@@ -100,40 +36,161 @@ const formatSection = (section) => {
   }
 };
 
+// show navbar section
+const sectionContainer = document.querySelector('.section-container');
+const section = ['home', 'world', 'politics', 'nyregion', 'technology', 'opinion', 'science', 'arts', 'books', 'style', 'food', 'travel', 'magazine', 'movies'];
+
+const sectionList = document.querySelector('.section-list');
+
+function showSectionList() {
+  section.forEach((m) => {
+    sectionList.innerHTML += `<li class="list-group-item">
+      <a class="text-decoration-none text-capitalize btn btn-light text-black" id="${m}">${formatSection(m)}</a>
+    </li>`;
+  });
+}
+
+showSectionList();
+
+// show footer section
+
+function showFooterList() {
+  const footerSection = document.querySelector('.footer-column');
+
+  section.forEach((m) => {
+    footerSection.innerHTML += `<li class="list-group-item"> 
+    <a class="text-decoration-none text-capitalize btn btn-light text-black" id="${m}" >${formatSection(m)}</a>
+    </li>`;
+  });
+
+  // Add event listener to each footer link
+  const footerLinks = document.querySelectorAll('.footer-column a');
+  footerLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    });
+  });
+}
+
+showFooterList();
+
+// Get News By Section
+const navLink = document.querySelectorAll('.btn-light');
+
+navLink.forEach((m) => {
+  m.addEventListener('click', () => {
+    const section = m.id;
+    console.log('🚀 ~ file: script.js:16 ~ m.addEventListener ~ section:', section);
+    getNewsBySection(section);
+    showSection(section);
+    sectionContainer.innerHTML = showSection(section);
+  });
+});
+
+const loading = document.querySelector('.loading');
+function showLoading() {
+  loading.classList.remove('hide');
+}
+function hideLoading() {
+  loading.classList.add('show');
+}
+
+// Home Section News
+async function getHomeNews() {
+  const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`);
+  const homeNews = await response.json();
+  let cards = '';
+  homeNews.results.forEach((m) => (cards += showHomeNews(m)));
+  container.innerHTML = cards;
+}
+
+async function getNewsBySection(section) {
+  showLoading();
+  try {
+    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`);
+    if (!response.ok) {
+      const message = 'Something went wrong';
+      container.innerHTML = message;
+      throw new Error('Something went wrong');
+    } else {
+      const news = await response.json();
+      let cards = '';
+      news.results.forEach((m) => (cards += showHomeNews(m)));
+      container.innerHTML = cards;
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoading();
+  }
+}
+
+getHomeNews();
+
+// Search News
+async function searchNews(keyword) {
+  const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=${API_KEY}`);
+  const searchList = await response.json();
+  let cards = '';
+  searchList.response.docs.forEach((m) => (cards += showSearchNews(m)));
+  container.innerHTML = cards;
+}
+
+// Get Date
+const dateContainer = document.querySelector('.current-date');
+
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  weekday: 'long',
+};
+const currentDate = new Date().toLocaleDateString('en-US', options);
+dateContainer.innerHTML = currentDate;
+
+// Show section title
+function showSection(section) {
+  const sectionTitle = section === 'home' ? '' : `<p class="fw-bold pt-5 border-bottom fs-2">${formatSection(section)} News</p>`;
+  return sectionTitle;
+}
+
 // Show Home Section News
 function showHomeNews(news) {
   return `
   ${
     news.title && news.multimedia && news.abstract && news.byline && news.section
-      ? ` <div class="news pb-4 border-bottom">
-  <div class="main-content">
-    <div class="d-flex flex-row justify-content-between">
-      <div class="section bg-dark-subtle p-2">${formatSection(news.section)}</div>
-      <div class="date text-black-50 p-2">${formatDate(news.published_date)}</div>
-    </div>
-    <div class="desc-image">
-      <div class="description">
-        <div class="title  fw-bold">
-          <a class="text-decoration-none text-black" href="${news.url}">${news.title}</a>
-        </div>
-        <div class="byline text-black-50"><p class="p-0 m-0">${news.byline}</p> </div>
-        <div class="abstract">
-          <p>${news.abstract}</p>
-        </div>
-      </div>
-      <div class="image">
-        <img src="${news.multimedia[1].url}" class="img-fluid" alt="" />
-      </div>
-      <div class="abstract-mobile">
-         <p>${news.abstract}</p>
-      </div>
-    </div>
-  </div>
-</div>`
+      ? ` 
+      <div class="news pb-4 border-bottom">
+          <div class="main-content">
+            <div class="d-flex flex-row justify-content-between">
+              <div class="section bg-dark-subtle p-2">${formatSection(news.section)}</div>
+              <div class="date text-black-50 p-2">${formatDate(news.published_date)}</div>
+            </div>
+            <div class="desc-image">
+              <div class="description">
+                <div class="title  fw-bold">
+                  <a class="text-decoration-none text-black" href="${news.url}">${news.title}</a>
+                </div>
+                <div class="byline text-black-50"><p class="p-0 m-0">${news.byline}</p> </div>
+                <div class="abstract">
+                  <p>${news.abstract}</p>
+                </div>
+              </div>
+              <div class="image">
+                <img src="${news.multimedia[1].url}" class="img-fluid" alt="" loading="lazy"/>
+              </div>
+              <div class="abstract-mobile">
+                <p>${news.abstract}</p>
+              </div>
+            </div>
+          </div>
+        </div>`
       : ''
-  }
-  
-  `;
+  }`;
 }
 
 // Show search news
@@ -161,7 +218,3 @@ function showSearchNews(search) {
         </div>
     </div>`;
 }
-// ${search.keywords
-//   .slice(0, 5)
-//   .map((m) => `<span class="badge bg-primary">${m.value}</span>`)
-//   .join('')}
